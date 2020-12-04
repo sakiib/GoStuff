f9570188b5b224095a4221c5d65a0597986c3e75
sakibalamin <sakibalamin@appscode.com>
2020-12-04 15:13:00 +0600

    basic struct of a function

--- Functions/main.go
+++ Functions/main.go
@@ -4,6 +4,12 @@ import (
 	"fmt"
 )
 
+// basic struct: func functionName(argument argument type, ) return type {}
+func sumOfTwoNumbers(x, y int) int {
+	return x + y
+}
+
 func main() {
 	fmt.Println("Functions!")
+	fmt.Println(sumOfTwoNumbers(10, 20))
 }
