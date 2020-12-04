e5e3d178a2d8a82f4e485d9feb1a723e8bb30abf
sakibalamin <sakibalamin@appscode.com>
2020-12-04 14:32:09 +0600

    2 dimensional slice added

--- ArraysSlicesMaps/main.go
+++ ArraysSlicesMaps/main.go
@@ -98,13 +98,19 @@ func main() {
 	fmt.Println(len(a), cap(a))
 
 	aa := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	bb := a[:]   // slice of all elements
-	cc := a[3:]  // slice from 4th element to end
-	dd := a[:6]  // slice first 6 elements, so basically [left, right), left is inclusive & right is exclusive
-	ee := a[3:6] // slice the 4th, 5th, and 6th elements, aa, bb, cc, dd, ee all point to same underlying data
+	bb := aa[:]   // slice of all elements
+	cc := aa[3:]  // slice from 4th element to end
+	dd := aa[:6]  // slice first 6 elements, so basically [left, right), left is inclusive & right is exclusive
+	ee := aa[3:6] // slice the 4th, 5th, and 6th elements, aa, bb, cc, dd, ee all point to same underlying data
 	fmt.Println(aa)
 	fmt.Println(bb)
 	fmt.Println(cc)
 	fmt.Println(dd)
 	fmt.Println(ee)
+
+	// 2D slices, how we store graph. e.g: vector <int> graph[N], each of the indices from [0, 10) have an empty slice with them
+	var graph [10][]int
+	for i := 0; i < 10; i++ {
+		fmt.Println(graph[i])
+	}
 }
