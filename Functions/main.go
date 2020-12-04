2a50bc11096818641a61036a43570b8e889e96eb
sakibalamin <sakibalamin@appscode.com>
2020-12-04 17:51:48 +0600

    added variadic function

--- Functions/main.go
+++ Functions/main.go
@@ -30,6 +30,18 @@ func multiValRetFunc() (int, int) {
 	return 5, 10
 }
 
+// variadic function, can take any number of arguments as the last parameter only
+// By using ... before the type name of the last parameter we can indicate that it takes zero or more of those parameters.
+// This is precisely how the fmt.Println function is implemented
+
+func sumUsingVariadicFunc(args ...int) int {
+	sum := 0
+	for _, value := range args {
+		sum += value
+	}
+	return sum
+}
+
 func main() {
 	fmt.Println("Functions!")
 	fmt.Println(sumOfTwoNumbers(10, 20))
@@ -40,4 +52,5 @@ func main() {
 	// a function can return multiple values, types must given in similar fashion
 	val1, val2 := multiValRetFunc()
 	fmt.Println(val1, val2)
+	fmt.Println(sumUsingVariadicFunc(1, 2, 3, 4, 5, 6, 7))
 }
