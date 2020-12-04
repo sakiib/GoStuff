69bdab10abb5ac9db2c923bae5fada9d8d71c570
sakibalamin <sakibalamin@appscode.com>
2020-12-04 15:39:39 +0600

    sum of a list of int

--- Functions/main.go
+++ Functions/main.go
@@ -9,7 +9,18 @@ func sumOfTwoNumbers(x, y int) int {
 	return x + y
 }
 
+func sumOfAList(tempList []int) int {
+	sum := 0
+	for _, val := range tempList {
+		fmt.Println("here: ", val)
+		sum += val
+	}
+	return sum
+}
+
 func main() {
 	fmt.Println("Functions!")
 	fmt.Println(sumOfTwoNumbers(10, 20))
+	slice1 := []int{1, 2, 3, 4}
+	fmt.Println(sumOfAList(slice1))
 }
