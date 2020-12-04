3a05111e8dcfdf3be2c23f8f7e2b8fb8749af1f7
sakibalamin <sakibalamin@appscode.com>
2020-12-04 14:47:38 +0600

    map usage added

--- ArraysSlicesMaps/main.go
+++ ArraysSlicesMaps/main.go
@@ -113,4 +113,36 @@ func main() {
 	for i := 0; i < 10; i++ {
 		fmt.Println(graph[i])
 	}
+
+	// declaration of a map [key, value]: [string, int] using the make function
+	mp := make(map[string]int)
+	mp["sakib"] = 1
+	mp["alamin"] = 2
+	mp["someone"] = 3
+	// printing the map using key, val pair & ranging over the map
+	for key, val := range mp {
+		fmt.Println(key, val)
+	}
+	fmt.Println(mp["non_existant_key"]) // so, non existent key gives zero, so how do we check if it's actually there or not!!!
+
+	// so, to ensure that the key exists in our map, ok must be equal to true
+	val, ok := mp["someone"]
+	if ok {
+		fmt.Println("key exists & it's value is", val)
+	} else {
+		fmt.Println("key doesn't exist in the map mp")
+	}
+	// this is cute!! so, here's a semicolon!! we can use this condition to check the existance & print the value if it's present
+	// remember, them scope of this value is inside the curly braces only
+	if value, ok := mp["someone"]; ok {
+		fmt.Println("val: ", value)
+	} else {
+		fmt.Println("...not present")
+	}
+	// for some reason if we don't actually need the value & we just only need to check the existance then we can use underscore(_)
+	if _, ok := mp["non_existant_key"]; ok {
+		fmt.Println("the key exists")
+	} else {
+		fmt.Println("...not present")
+	}
 }
