3dc62f0b2c67eb4638e051017dd41b2c58bce43e
sakibalamin <sakibalamin@appscode.com>
2020-12-04 17:56:59 +0600

    min & max func using variadic function

--- Functions/main.go
+++ Functions/main.go
@@ -42,6 +42,29 @@ func sumUsingVariadicFunc(args ...int) int {
 	return sum
 }
 
+// no built-int function for calculating minimum, variadic function can be used here to implement one
+// it takes two or more arguments & calculates the minimum value among them, max can be implemented similarly
+
+func min(x int, args ...int) int {
+	mn := x
+	for _, value := range args {
+		if value < mn {
+			mn = value
+		}
+	}
+	return mn
+}
+
+func max(x int, args ...int) int {
+	mx := x
+	for _, value := range args {
+		if value > mx {
+			mx = value
+		}
+	}
+	return mx
+}
+
 func main() {
 	fmt.Println("Functions!")
 	fmt.Println(sumOfTwoNumbers(10, 20))
@@ -53,4 +76,5 @@ func main() {
 	val1, val2 := multiValRetFunc()
 	fmt.Println(val1, val2)
 	fmt.Println(sumUsingVariadicFunc(1, 2, 3, 4, 5, 6, 7))
+	fmt.Println(min(5, 4, 2, 1, 3), max(1, 4, 5, 2, 3))
 }
