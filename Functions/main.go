7a1d3ab26a4a0bb3e499ee43f4527927eb77d082
sakibalamin <sakibalamin@appscode.com>
2020-12-04 17:46:59 +0600

    some functions variations by return type

--- Functions/main.go
+++ Functions/main.go
@@ -18,9 +18,26 @@ func sumOfAList(tempList []int) int {
 	return sum
 }
 
+func func2() int {
+	return 100
+}
+
+func func1() int {
+	return func2()
+}
+
+func multiValRetFunc() (int, int) {
+	return 5, 10
+}
+
 func main() {
 	fmt.Println("Functions!")
 	fmt.Println(sumOfTwoNumbers(10, 20))
 	slice1 := []int{1, 2, 3, 4}
 	fmt.Println(sumOfAList(slice1))
+	// functions are built-up in a stack like fashion
+	fmt.Println(func1())
+	// a function can return multiple values, types must given in similar fashion
+	val1, val2 := multiValRetFunc()
+	fmt.Println(val1, val2)
 }
