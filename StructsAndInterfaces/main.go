bbcc45003567bcf5d67c2c9b687e39b78d25c904
sakibalamin <sakibalamin@appscode.com>
2020-12-07 11:52:24 +0600

    struct basic added

--- StructsAndInterfaces/main.go
+++ StructsAndInterfaces/main.go
@@ -4,6 +4,19 @@ import (
 	"fmt"
 )
 
+// Student struct
+type Student struct {
+	ID        string
+	FirstName string
+	LastName  string
+	CG        float32
+	Credits   int
+}
+
 func main() {
 	fmt.Println("structs and interfaces!")
+	student1 := Student{ID: "123", FirstName: "sakib", LastName: "alamin", CG: 3.5, Credits: 130}
+	// %v prints only the values, if we need the name of the fields, we can use %+v to achieve that
+	fmt.Printf("%+v\n", student1)
+	fmt.Println(student1)
 }
