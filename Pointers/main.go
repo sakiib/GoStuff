a3e13f1cfa4114740873e5455b524eca8c8fbf47
sakibalamin <sakibalamin@appscode.com>
2020-12-07 09:50:34 +0600

    basic example with/out ref added

--- Pointers/main.go
+++ Pointers/main.go
@@ -4,6 +4,20 @@ import (
 	"fmt"
 )
 
+func setValWithourReff(val int) {
+	val = 0
+}
+
+func setValWithReff(val *int) {
+	*val = 0
+}
+
 func main() {
 	fmt.Println("Pointers!")
+	currentVal := 10
+	fmt.Println(currentVal)
+	setValWithourReff(currentVal)
+	fmt.Println("without pointer reference: ", currentVal)
+	setValWithReff(&currentVal)
+	fmt.Println("with pointer reference: ", currentVal)
 }
