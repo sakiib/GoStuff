52529130c2181b9ef24fcc0a535e258f97a4d06d
sakibalamin <sakibalamin@appscode.com>
2020-12-07 10:25:53 +0600

    stack added basic op

--- DataStructures/main.go
+++ DataStructures/main.go
@@ -6,4 +6,22 @@ import (
 
 func main() {
 	fmt.Println("Data Structures!")
+
+	// delcaring a stack of integer using make & slice
+	stack := make([]int, 0)
+	// appending or adding values to the stack
+	stack = append(stack, 1)
+	stack = append(stack, 2)
+	stack = append(stack, 5)
+	stack = append(stack, 10)
+	stack = append(stack, 0)
+	fmt.Println(stack, len(stack))
+
+	// printing a stack
+	for len(stack) > 0 {
+		topIndex := len(stack) - 1   // index of the value that is our current top of the stack
+		fmt.Println(stack[topIndex]) // printing the top of the stack
+		stack = stack[:topIndex]     // updating the stacking, exluding the stack or simply we're popping the top of the stack
+	}
+
 }
