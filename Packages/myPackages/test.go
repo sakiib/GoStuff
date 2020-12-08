9175de01fa3fc9d231dbfa52052a651ea382b00c
sakibalamin <sakibalamin@appscode.com>
2020-12-08 12:04:31 +0600

    min, max using variadic function

--- Packages/myPackages/test.go
+++ Packages/myPackages/test.go
@@ -1,15 +1,29 @@
 package myPackages
 
-func Min(x, y int) int {
-	if x > y {
-		return x
+// variadic function usage: takes two or more arguments & finds the maximum among them
+func Max(a int, b int, args ...int) int {
+	mx := a
+	if b > mx {
+		mx = b
 	}
-	return y
+	for _, val := range args {
+		if val > mx {
+			mx = val
+		}
+	}
+	return mx
 }
 
-func Max(x, y int) int {
-	if x < y {
-		return x
+// variadic function usage: takes two or more arguments & finds the minimum among them
+func Min(a int, b int, args ...int) int {
+	mn := a
+	if b < mn {
+		mn = b
 	}
-	return y
+	for _, val := range args {
+		if val < mn {
+			mn = val
+		}
+	}
+	return mn
 }
