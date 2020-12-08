d6cf95f9483d6718bcfa947ebc998a9ef9da6615
sakibalamin <sakibalamin@appscode.com>
2020-12-08 11:54:40 +0600

    package added

--- Packages/main.go
+++ Packages/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"github.com/sakiib/GoStuff/Packages/myPackages"
 )
 
 func main() {
 	fmt.Println("Packages!")
+	fmt.Println(myPackages.Min(10, 20), myPackages.Max(10, 20))
 }
