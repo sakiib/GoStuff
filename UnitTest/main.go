a0e471e77e08e86ee26ef9fc6f3b1357dd016d19
sakibalamin <sakibalamin@appscode.com>
2020-12-09 16:30:35 +0600

    unittest updated

--- UnitTest/main.go
+++ UnitTest/main.go
@@ -5,6 +5,8 @@ import "fmt"
 func main() {
 	fmt.Println("Unit Test!")
 	fmt.Println(Total(1, 3, 5, 6))
+	fmt.Println(Min(20, 500))
+	fmt.Println(Max(10, 30))
 }
 
 func Total(args ...int) int {
@@ -13,4 +15,18 @@ func Total(args ...int) int {
 		sum += val
 	}
 	return sum
+}
+
+func Min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+func Max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
 }
--- UnitTest/main_test.go
+++ UnitTest/main_test.go
@@ -13,24 +13,90 @@ func TestTotal(t *testing.T) {
 	}{
 		// TODO: Add test cases.
 		{
-			name: "first test",
+			name: "first test case Total",
 			args: args{
 				[]int{1, 2, 3},
 			},
 			want: 6,
 		},
 		{
-			name: "second test",
+			name: "second test case Total",
 			args: args{
 				args: []int{2, 3, 4},
 			},
-			want: 10,
+			want: 9,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := Total(tt.args.args...); got != tt.want {
 				t.Errorf("Total() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestMin(t *testing.T) {
+	type args struct {
+		x int
+		y int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "first test case Min func",
+			args: args{
+				x: 10,
+				y: 20,
+			},
+			want: 10,
+		},
+		{
+			name: "second test case MIn func",
+			args: args{
+				x: 100,
+				y: 50,
+			},
+			want: 50,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Min(tt.args.x, tt.args.y); got != tt.want {
+				t.Errorf("Min() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestMax(t *testing.T) {
+	type args struct {
+		x int
+		y int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "first test case for max func",
+			args: args{
+				x: 5,
+				y: 4,
+			},
+			want: 5,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Max(tt.args.x, tt.args.y); got != tt.want {
+				t.Errorf("max() = %v, want %v", got, tt.want)
 			}
 		})
 	}
