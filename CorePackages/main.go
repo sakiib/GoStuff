3877942dbc7cf9c6268765474b72b606210f0114
sakibalamin <sakibalamin@appscode.com>
2020-12-10 15:47:17 +0600

    added corepackages & file IO upd.

---
+++ CorePackages/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	fmt.Println("Core packages!")
+	// strings
+	// true/false: contains as substring or not.
+	text := "sakib"
+	pattern := "ki"
+	fmt.Println(strings.Contains(text, pattern))
+
+	// int: counts the number of times st occurs as substring in name
+	name := "aabbaaccdd"
+	st := "aa"
+	fmt.Println(strings.Count(name, st))
+
+	// true/false: checks if a string has occurred as a prefix of another string
+	prefixTest := "appscode"
+	prefix := "apps"
+	fmt.Println(strings.HasPrefix(prefixTest, prefix))
+
+	// true/false: checks if a string has occurred as a prefix of another string
+	suffixTest := "appscode"
+	suffix := "apps"
+	fmt.Println(strings.HasPrefix(suffixTest, suffix))
+
+	// first occurrenece index if present, -1 otherwise
+	indexTest := "sakibalamin"
+	fmt.Println(strings.Index(indexTest, "x"))
+
+	fmt.Println(strings.Join([]string{"abc","def"}, "xyz")) // output: abcxyzdef
+	fmt.Println(strings.Repeat("sakib", 3)) // repeat this string 3 times sakibsakibsakib
+
+	// output: bbbkkbba, replaces first 5 occurrences of a by b
+	// replaces the substring with the value given, need not to be of same length
+	fmt.Println(strings.Replace("aaakkaaa", "a", "b", 5))
+	fmt.Println(strings.Replace("aabaab", "aab", "x", 2))
+
+	// split by the second param string: output: xyz, returns a list of string
+	charList := strings.Split("xabyabz", "ab")
+	fmt.Println(charList)
+	fmt.Println(strings.Split("a-b-c-d-e", "-"))
+
+	// to change case of string
+	fmt.Println(strings.ToLower("SAKIB"), strings.ToUpper("alamin"))
+
+	// converting a string to a slice of binary data, [116 101 115 116] & "test"
+	arr := []byte("test")
+	fmt.Println(arr)
+	str := string([]byte{'t','e','s','t'})
+	fmt.Println(str)
+	// see the fileIO part for file handling code & frequently used part for sorting array or slices
+
+	
+}
--- FileIO/main.go
+++ FileIO/main.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"path/filepath"
 )
 
 func main() {
@@ -32,4 +33,68 @@ func main() {
 			fmt.Print(string(val))
 		}
 	}
+
+	// according to the book..
+	file, err := os.Open("test.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	// use defer file.Close() right after opening the file to make sure the file is closed as soon as the function completes.
+	// see the definitions of defer if needed
+
+		// get the file size
+	stat, err := file.Stat()
+	if err != nil {
+		log.Fatal(err)
+	}
+	// read the file
+	bs := make([]byte, stat.Size())
+	_, err = file.Read(bs)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	str := string(bs)
+	fmt.Println(str)
+
+	// using the ioutil
+	fs, err := ioutil.ReadFile("test.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	st := string(fs)
+	fmt.Println(st)
+
+	// Creating a file
+	cfile, err := os.Create("test.txt")
+	if err != nil {
+		// handle the error here
+		return
+	}
+	defer cfile.Close()
+	file.WriteString("test")
+
+	dir, err := os.Open(".")
+	if err != nil {
+		return
+	}
+	defer dir.Close()
+
+	// To get the contents of a directory we use the same os.
+	//Open function but give it a directory path instead of a file name. Then we call the Readdir method
+	fileInfos, err := dir.Readdir(-1)
+	if err != nil {
+		return
+	}
+	for _, fi := range fileInfos {
+		fmt.Println(fi.Name())
+	}
+
+	// Often we want to recursively walk a folder (read the folder's contents, all the sub-folders, all the
+	// sub-sub-folders, …). To make this easier there's a Walk function provided in the path/filepath package
+	filepath.Walk(".", func(path string, info os.FileInfo, err error) error {
+		fmt.Println(path)
+		return nil
+	})
 }
